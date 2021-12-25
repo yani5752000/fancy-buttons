c4d91bb38fc7cad3c3d58e8008d2a35662aa7abd
Farid <yani5752000@yahoo.com>
2021-12-25 13:47:14 -0500

    re-organisied the states

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import TextRepeaterButton from './components/TextRepeaterButton';
 function App() {
   const [light, setLight] = useState('off');
   const dark = (light === "off") ? "dark" : "";
+  const switchLight = () => setLight(light === "on" ? "off" : "on");
 
   return (
     <div className={`App ${dark}`}>
@@ -15,7 +16,7 @@ function App() {
       <section>
         <AngryButton />
         <CounterButton />
-        <LightSwitchButton light={light} setLight={setLight} />
+        <LightSwitchButton light={light} switchLight={switchLight} />
         <TextRepeaterButton />
       </section>
     </div>
--- src/components/LightSwitchButton.js
+++ src/components/LightSwitchButton.js
@@ -1,11 +1,11 @@
 import React from "react";
 
 function LightSwitchButton(props) {
-  const {light, setLight} = props;
-  const handleClick = () => setLight(light === "on" ? "off" : "on");
+  const {light, switchLight} = props;
+  
 
   return (
-    <button onClick={handleClick} className="LightSwitchButton">
+    <button onClick={switchLight} className="LightSwitchButton">
       {light === 'on' && <span><i>💡</i> I'm on!</span>}
       {light === 'off' && <span className="off"><i>💡</i> I'm off!</span>}
     </button>
